0b745dcbd4062451882d6aa6732633665fcae6b4
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-07-15 18:47:45 +0200

    🔥 Remove ability to cancel

--- dist/script.js
+++ dist/script.js
@@ -2,25 +2,46 @@
 (() => {
     const params = new URLSearchParams(location.search);
     const redirect = params.get("uddg");
-    if (redirect === null) {
-        console.warn('NoLBang: Got a search for "l" without a parameter');
+    if (redirect === null || params.get("disablenolbang") !== null) {
         return;
     }
+    let toCancel = true;
     const wrapper = document.createElement("div");
     wrapper.style.margin = "1rem";
     wrapper.style.fontSize = "1.5rem";
-    const initial = document.createTextNode("Redirecting to ");
+    const initial = document.createTextNode("You will get redirected to ");
     const link = document.createElement("a");
     link.href = redirect;
     link.textContent = redirect;
     link.style.color = "#769dff";
-    const last = document.createTextNode("...");
+    const last = document.createTextNode(" in about a second.");
+    const cancel = document.createElement("button");
+    cancel.textContent = "Go back!";
+    cancel.style.background = "transparent";
+    cancel.style.font = "inherit";
+    cancel.style.color = "inherit";
+    cancel.style.border = "0";
+    cancel.style.padding = "0";
+    cancel.style.display = "block";
+    cancel.style.cursor = "pointer";
+    cancel.style.textDecoration = "underline";
+    cancel.addEventListener("click", () => {
+        toCancel = false;
+        const newUrl = new URL(location.href);
+        newUrl.searchParams.set("disablenolbang", "");
+        location.href = newUrl.toString();
+    });
     wrapper.appendChild(initial);
     wrapper.appendChild(link);
     wrapper.appendChild(last);
+    wrapper.appendChild(cancel);
     document.body.style.backgroundColor = "#161616";
     document.body.style.color = "#eeeeee";
     document.body.textContent = "";
     document.body.appendChild(wrapper);
-    location.href = redirect;
+    setTimeout(() => {
+        if (toCancel) {
+            location.href = redirect;
+        }
+    }, 1500);
 })();
