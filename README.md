4c0f9a7702c0afb3816b39240d813031bfbd15d6
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-07-15 18:10:21 +0200

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# NoLBang
+
+DuckDuckGo has an annoying issue that when using almost any bang, you aren't redirected to the actual URL, but instead to a DuckDuckGo search for the letter L. Try it: <https://duckduckgo.com/?q=cat+!w>
+
+However, the actual URL is still encoded in the path - they probably accidentally broke it. This allows me to create an extension that fixes this, though!
+
+## Installation
+
+This is not in any web stores. You will have to clone the repository and enable the extension in whatever way your browser does this. I have not tested this in anything but Edge, but it should work in any Chromium-based browser (Firefox doesn't have a good way to run extensions like this).
---
+++ src/script.ts
@@ -0,0 +1,25 @@
+(() => {
+  const params = new URLSearchParams(location.search);
+  const redirect = params.get("uddg");
+  if (redirect === null) {
+    console.warn('NoLBang: Got a search for "l" without a parameter');
+    return;
+  }
+  const wrapper = document.createElement("div");
+  wrapper.style.margin = "1rem";
+  wrapper.style.fontSize = "1.5rem";
+  const initial = document.createTextNode("Redirecting to ");
+  const link = document.createElement("a");
+  link.href = redirect;
+  link.textContent = redirect;
+  link.style.color = "#769dff";
+  const last = document.createTextNode("...");
+  wrapper.appendChild(initial);
+  wrapper.appendChild(link);
+  wrapper.appendChild(last);
+  document.body.style.backgroundColor = "#161616";
+  document.body.style.color = "#eeeeee";
+  document.body.textContent = "";
+  document.body.appendChild(wrapper);
+  location.href = redirect;
+})();
